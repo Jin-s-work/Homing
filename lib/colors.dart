39f5b7c2f186c82e50a9b222cc21a152d34a62c9
Jin-s-work <9114jin@hanmail.net>
2024-06-19 15:50:06 +0900

    First Page

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFF1EFED);
+  static const Color greyButton = Color(0xFFB6B1AB);
+  static const Color blackButton = Color(0xFF5C6169);
+  static const Color yellowButton = Color(0xFFF0CC71);
+  static const Color orangeButton = Color(0xFFF77E47);
+}
